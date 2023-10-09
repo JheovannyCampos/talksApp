90f7cf143121451c80930ca78fe6c5bcc62e04c4
Jheovanny <jheovanny06@hotmail.com>
2023-10-09 14:17:57 -0300

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import ChatPage from "./components/ChatPage";
+import socketIO from "socket.io-client";
+
+const backendURL =
+  process.env.NODE_ENV === "production"
+    ? "https://chat.vercel.app"
+    : "http://localhost:4000";
+
+const socket = socketIO.connect(backendURL);
+function App() {
+  return (
+    <BrowserRouter>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home socket={socket} />}></Route>
+          <Route path="/chat" element={<ChatPage socket={socket} />}></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
